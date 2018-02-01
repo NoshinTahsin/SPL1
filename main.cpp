116fb3237edb691400192ff72627399998cd0c06
NoshinTahsin <noshintahsiniitdu@gmai9l.com>
2018-02-01 14:30:37 +0600

    saaj

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int arr[3];
+    for(int i=10;i>=0;i--)
+    {
+           int x=5/i;
+           cout<<x<<endl;
+    }
+
+
+    return 0;
+}
